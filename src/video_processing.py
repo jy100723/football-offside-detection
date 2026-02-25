f41106c2c94fc6679c6211ce74551293ef5fe18b
jy100723 <jy100723@gmail.com>
2026-02-26 00:04:07 +0900

    Create video_processing module

---
+++ src/video_processing.py
@@ -0,0 +1,8 @@
+# video_processing.py
+
+class VideoProcessor:
+    def __init__(self, video_path):
+        self.video_path = video_path
+
+    def process_video(self):
+        pass  # Add your video processing logic here
